533eaae580ebdaa07f138adc4d41f964bfd06c14
Clayton Davis <clayadavis@gmail.com>
2014-11-10 19:07:29 -0500

    Simplify and improve mobile

--- assets/global.js
+++ assets/global.js
@@ -1,1 +1,11 @@
-
+window.onload = function() {
+  var $button, $nav;
+  $button = document.querySelector('.nav-button');
+  $nav = document.querySelector('nav');
+  $button.onclick = function() {
+    return $nav.style.display = $nav.style.display === 'table' ? 'none' : 'table';
+  };
+  return $nav.onclick = function() {
+    return $nav.style.display = 'none';
+  };
+};
