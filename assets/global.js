4059453b387b98fa09a71793501a540746b13b92
Clayton Davis <clayadavis@gmail.com>
2015-04-24 14:36:28 -0700

    Update banner

--- assets/global.js
+++ assets/global.js
@@ -1,1 +1,11 @@
-window.onload=function(){var n,e;n=document.querySelector(".nav-button");e=document.querySelector("nav");n.onclick=function(){return e.style.display=e.style.display==="table"?"none":"table"};return e.onclick=function(){return e.style.display="none"}};
+window.onload = function() {
+  var $button, $nav;
+  $button = document.querySelector('.nav-button');
+  $nav = document.querySelector('nav');
+  $button.onclick = function() {
+    return $nav.style.display = $nav.style.display === 'table' ? 'none' : 'table';
+  };
+  return $nav.onclick = function() {
+    return $nav.style.display = 'none';
+  };
+};
